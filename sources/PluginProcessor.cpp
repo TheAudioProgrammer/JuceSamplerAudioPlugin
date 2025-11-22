eb0b868833e81313740f291f4a76e890d9043028
Joshua Hodge <josh@theaudioprogrammer.com>
2025-11-22 10:19:42 -0500

    Demonstrate BigInteger to pitch samples up or down

--- sources/PluginProcessor.cpp
+++ sources/PluginProcessor.cpp
@@ -10,50 +10,33 @@ SamplerAudioProcessor::SamplerAudioProcessor() :
         synth.addVoice(new juce::SamplerVoice);
     }
 
-    //! LOAD ONE SOUND FROM BINARY DATA
-
-    /* 1. The AudioFormatManager adds the ability to handle different types of audio formats, such as WAV and AIFF
-     * AudioFormatManager Class: https://docs.juce.com/master/classjuce_1_1AudioFormatManager.html */
-
     juce::AudioFormatManager formatManager;
-
-    // 2. You must call registerBasicFormats() so the format manager can register handlers for these file types
-
     formatManager.registerBasicFormats();
-
-    /* 3. First we need the sound we're trying to load.  We can use the MemoryInputStream class
-     * This would first start as auto sound = juce::MemoryInputStream(BinaryData::c5_wav, BinaryData::c5_wavSize, false);
-     * It needs to become a unique ptr later because createReaderFor() takes in a unique ptr
-     * MemoryInputStream Class: https://docs.juce.com/master/classjuce_1_1MemoryInputStream.html */
 
     auto inputStream = std::make_unique<juce::MemoryInputStream>(BinaryData::c5_wav,
                                                                BinaryData::c5_wavSize,
                                                                false);
 
-    /* 4. Now that we've registered the formats, we use a JUCE MemoryInputStream to try and read in the first sound
-     * We use std::move to pass ownership from our PluginProcessor class to the Format Manager.
-     * Otherwise, both the PluginProcessor and AudioFormat objects would own the inputStream...forbidden by unique ptr
-     * Remember to check that the reader is not null before using it
-     * MemoryInputStream Class: https://docs.juce.com/master/classjuce_1_1MemoryInputStream.html */
-
     if (auto reader = formatManager.createReaderFor(std::move(inputStream)))
     {
-        // 6. I'll add some local variables because I will want to bring this out into its own function later
         const juce::String name = "C5";
-        int originalMidiNote = 60; // Middle C
+        int originalMidiNote = 60;
 
-        // 7. This enables us to be able to load the sound at multiple pitches if we want.
-        // Think of BigInteger as a set of all 128 midi notes, and I can switch each one on or off to trigger the same sample
-        juce::BigInteger midiNote;
-        midiNote.setBit(originalMidiNote);
+        /* 1. Now let's demo how we can use this sample for multiple notes with a vector of notes we want the sample
+         * to trigger for.  This is where the original midi note variable comes into play.  The engine will use this
+         * to determine how much to pitch up / down the original sample to play in key. */
+        std::vector<int> midiNoteSet = { 60, 61, 62, 63, 64, 65, 66, 67 };
 
-        /* 5. Now we create a SamplerSound that contains our wav file data and assigns it to a midi note with attack, release
-         * and max sample length of time (in seconds)
-         * SamplerSound Class: https://docs.juce.com/master/classjuce_1_1SamplerSound.html */
+        juce::BigInteger midiNotes;
 
-        auto sound = new juce::SamplerSound(name, *reader, midiNote, originalMidiNote, 0.0, 0.1, 10.0);
+        // Now we turn on all the midi notes we want to trigger with this sample
+        for (auto note : midiNoteSet)
+        {
+            midiNotes.setBit(note);
+        }
 
-        // 6. Now we'll load the sound into the synth
+        auto sound = new juce::SamplerSound(name, *reader, midiNotes, originalMidiNote, 0.0, 0.1, 10.0);
+
         synth.addSound(sound);
     }
 }
