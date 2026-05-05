077d474de68acf4a27657c6d297de5c67e9192f6
Joshua Hodge <josh@theaudioprogrammer.com>
2026-05-05 11:16:35 -0400

    Creating LookAndFeel

---
+++ sources/SamplerLookAndFeel.h
@@ -0,0 +1,15 @@
+//
+// Created by Joshua Hodge on 5/5/26.
+//
+
+#pragma once
+
+#include <juce_graphics/juce_graphics.h>
+
+// 1. We're going to add some colors for use later
+namespace Colors
+{
+    const juce::Colour textBright { 209, 209, 209 };
+    const juce::Colour textMid { 178, 179, 178 };
+    const juce::Colour textDim { 156, 157, 156 };
+}
