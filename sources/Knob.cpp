d56f49977990f18a1e11c1e3ac52bd353060f14a
Joshua Hodge <josh@theaudioprogrammer.com>
2026-06-22 10:34:18 -0400

    Adding Knob class

---
+++ sources/Knob.cpp
@@ -0,0 +1,28 @@
+//
+// Created by Joshua Hodge on 6/22/26.
+//
+
+#include "Knob.h"
+
+Knob::Knob(const juce::String& text,
+           juce::AudioProcessorValueTreeState& apvts,
+           const juce::ParameterID& parameterID) :
+    sliderLF(Images::getKnob(), 129), attachment(apvts, parameterID.getParamID(), slider)
+{
+    slider.setSliderStyle(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag);
+    slider.setLookAndFeel(&sliderLF);
+    addAndMakeVisible(slider);
+
+    label.setText(text, juce::NotificationType::dontSendNotification);
+    label.setJustificationType(juce::Justification::horizontallyCentred);
+    label.setFont(Fonts::getBoldFont());
+    label.setColour(juce::Label::textColourId, Colors::textMid);
+    addAndMakeVisible(label);
+}
+
+void Knob::resized()
+{
+    label.setBounds(0, 0, getWidth(), 19);
+    slider.setTextBoxStyle(juce::Slider::TextBoxBelow, false, getWidth(), 14);
+    slider.setBounds(0, 19, getWidth(), 88);
+}
---
+++ sources/Knob.h
@@ -0,0 +1,30 @@
+//
+// Created by Joshua Hodge on 6/22/26.
+//
+
+#pragma once
+
+#include "SamplerLookAndFeel.h"
+#include "juce_audio_processors/juce_audio_processors.h"
+
+
+class Knob : juce::Component
+{
+public:
+    Knob (const juce::String& text,
+        juce::AudioProcessorValueTreeState& apvts,
+        const juce::ParameterID& parameterID);
+
+    void resized() override;
+
+    juce::Slider& getSlider() { return slider; }
+    juce::Label& getLabel() { return label; }
+
+private:
+    KnobLookAndFeel sliderLF;
+    juce::Slider slider;
+    juce::Label label;
+    juce::AudioProcessorValueTreeState::SliderAttachment attachment;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(Knob)
+};
